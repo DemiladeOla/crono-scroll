1d4ecfa14077fa956b9fadd9218d213b63f19234
DemiladeOla <97028307+DemiladeOla@users.noreply.github.com>
2022-04-06 16:30:36 -0400

    Correct code

--- processing/art-scroller/art-scroller.ino
+++ processing/art-scroller/art-scroller.ino
@@ -1,19 +1,23 @@
- import processing.serial.*;
+import processing.serial.*; 
 Serial myPort;
 
 int numOfImages = 10;
 float distance = 0;
 float smoothDistance = 0;
+float smoothIndicatorPos = 0;
+int lineUnit = 0;
+int currentIndex = numOfImages;
+PFont fontFamily;
 PImage[] images = new PImage[numOfImages];
 Artwork[] artworks = new Artwork[numOfImages];
 String[] dates = {"600 B.C", "510 B.C", "500 B.C", "490 B.C", "400 B.C", "300 B.C", "275 B.C", "250 B.C", "200 B.C", "100 A.D"};
-int lineUnit;
 
 
 void setup() {
   imageMode(CENTER);
-  size(displayWidth, displayHeight);
+  
   printArray(Serial.list());
+  size(displayWidth, displayHeight);
   
   String portName = Serial.list()[2];
   myPort = new Serial(this, portName, 9600);
@@ -24,6 +28,8 @@ void setup() {
     artworks[i] = new Artwork(images[i]);
   }
   lineUnit = height/(numOfImages + 1);
+  fontFamily = createFont("Druk", 88);
+  textFont(fontFamily);
 }
 
 void draw() {
@@ -31,18 +37,20 @@ void draw() {
   fill(100);
   noStroke();
   drawTimeline();
-  smoothDistance = constrain(lerp(smoothDistance, distance, 0.1), 200, 1000);
-  int activeIndex = round(map(smoothDistance, 200, 1000, 0, 9));
+  smoothDistance = constrain(lerp(smoothDistance, distance, 0.1f), 200, 1000);
+  int activeIndex = floor(map(smoothDistance, 200, 1000, 0, 9));
   showArtwork(activeIndex);
   fill(255, 100, 50);
   float indicatorPos = map(smoothDistance, 200, 1000, lineUnit, height-lineUnit);
-  rect(0, indicatorPos, 80, 4);
+  smoothIndicatorPos = lerp(smoothIndicatorPos, indicatorPos, 0.1f);
+  rect(0, smoothIndicatorPos - (lineUnit/3), 88, 4);
   fill(50);
-  textSize(32);
-  text(dates[activeIndex], 120, indicatorPos);
+  text(dates[activeIndex], 128, smoothIndicatorPos);
 }
 
 void showArtwork(int index) {
+  int prevIndex = index >= numOfImages - 1 ? 0 : index + 1;
+  artworks[prevIndex].hide();
   int nextIndex = index - 1 >= 0 ? index - 1 : numOfImages - 1;
   artworks[nextIndex].prepare();
   artworks[index].display();
@@ -50,20 +58,28 @@ void showArtwork(int index) {
 
 void drawTimeline() {
   for (int i=0; i < numOfImages; i++) {
-    rect(0, lineUnit*(i+1), 24, 4); 
+    rect(0, lineUnit*(i+1), 40, 4); 
   }
 }
 
 void serialEvent(Serial myPort) {
   String myString = myPort.readStringUntil('\n');
   if(myString != null){
-    distance = float(myString);
+    distance = PApplet.parseFloat(myString);
   }
 }
 class Artwork {
   PImage image;
   float x = width/2;
-  float y = height/2.1;
+  float y = height/2.1f;
+  float prepX = x + width/3.5f;
+  float holdX = prepX;
+  float holdX2 = width;
+  float hideX = x - width/3.5f;
+  float holdHideX = x;
+  float alpha = 60;
+  float holdAlpha = alpha;
+  float holdAlpha2 = 255;
   
   Artwork(PImage image){
     this.image = image;
@@ -71,12 +87,31 @@ class Artwork {
   }
   
   void prepare(){
-    tint(255, 60);
-    image(this.image, this.x + width/4, this.y);
-    tint(255, 255);
+    tint(255, alpha);
+    this.holdX2 = lerp(this.holdX2, this.prepX, 0.12f);
+    image(this.image, this.holdX2, this.y);
+    
+    this.holdX = prepX;
+    this.holdAlpha = this.alpha;
   }
   
   void display(){
-    image(this.image, this.x, this.y);
+    this.holdAlpha = lerp(this.holdAlpha, 255, 0.1f);
+    tint(255, this.holdAlpha);
+    this.holdX = lerp(this.holdX, this.x, 0.1f);
+    image(this.image, this.holdX, this.y);
+    
+    this.holdHideX = this.x;
+    this.holdAlpha2 = 255;
+  }
+  
+  void hide(){
+    this.holdAlpha2 = lerp(this.holdAlpha2, 20, 0.08f);
+    tint(255, this.holdAlpha2); 
+    this.holdHideX = lerp(this.holdHideX, this.hideX, 0.08f);
+    image(this.image, this.holdHideX, this.y);
+    
+    this.holdX2 = width;
   }
 }
+
